79b6c36ae2defc94bd1b97208fe32b1d833aa97b
46kuri_ <ghost.46kuri@gmail.com>
2022-03-21 13:47:21 +0900

    Add files via upload

--- src/main/java/com/github/sirokuri_/junksurvival/JunkSurvival.java
+++ src/main/java/com/github/sirokuri_/junksurvival/JunkSurvival.java
@@ -1,8 +1,5 @@
 package com.github.sirokuri_.junksurvival;
 
-import co.aikar.taskchain.BukkitTaskChainFactory;
-import co.aikar.taskchain.TaskChain;
-import co.aikar.taskchain.TaskChainFactory;
 import com.github.sirokuri_.junksurvival.listeners.JunkSurvivalGiantSpawnListener;
 import com.github.sirokuri_.junksurvival.listeners.JunkSurvivalItemDropListener;
 import com.github.sirokuri_.junksurvival.listeners.JunkSurvivalMessageListener;
@@ -12,27 +9,17 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class JunkSurvival extends JavaPlugin {
 
-    private static TaskChainFactory taskChainFactory;
-
     @Override
     public void onEnable() {
-        taskChainFactory = BukkitTaskChainFactory.create(this);
         Bukkit.getPluginManager().registerEvents(new JunkSurvivalItemDropListener(), this);
         Bukkit.getPluginManager().registerEvents(new JunkSurvivalMessageListener(), this);
         Bukkit.getPluginManager().registerEvents(new JunkSurvivalPreventGriefListener(), this);
+        Bukkit.getPluginManager().registerEvents(new JunkSurvivalGiantSpawnListener(), this);
         Bukkit.getLogger().info(getName() + " enabled.");
     }
 
     @Override
     public void onDisable() {
         Bukkit.getLogger().info(getName() + " disabled.");
-    }
-
-    public static <T> TaskChain<T> newChain() {
-        return taskChainFactory.newChain();
-    }
-
-    public static <T> TaskChain<T> newSharedChain(String name) {
-        return taskChainFactory.newSharedChain(name);
     }
 }
